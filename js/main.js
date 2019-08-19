7a573176261f2f646e3951f41ef0768508a5e460
Marina Chukhnova <marina_chukhnova@ukr.net>
2019-08-19 23:20:19 +0300

    JS slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+/*var slider = {
+	slides:['img/img3.jpg','img/img4.jpg','img/img3.jpg','img/img4.jpg'],
+	frame:0, // текущий кадр для отбражения - индекс картинки из массива
+	set: function(image) { // установка нужного фона слайдеру
+		document.getElementById("scr").style.backgroundImage = "url("+image+")";
+	},
+	init: function() { // запуск слайдера с картинкой с нулевым индексом
+		this.set(this.slides[this.frame]);
+	},
+	left: function() { // крутим на один кадр влево
+		this.frame--;
+		if(this.frame < 0) this.frame = this.slides.length-1;
+		this.set(this.slides[this.frame]);
+	},
+	right: function() { // крутим на один кадр вправо
+		this.frame++;
+		if(this.frame == this.slides.length) this.frame = 0;
+		this.set(this.slides[this.frame]);		
+	}
+};
+window.onload = function() { // запуск слайдера после загрузки документа
+	slider.init();
+	setInterval(function() { // ставим пятисекундный интервал для перелистывания картинок
+	slider.right();
+	} ,5000);
+};*/
+
+
 $(function() {
 	$('.works-carousel').owlCarousel({
 	    loop:true,
